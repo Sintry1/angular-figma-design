8562858652ae9c4a8397d1e765e7fb2adf889bca
Dominic Smith <Dominic_smith2012@hotmail.co.uk>
2022-08-29 14:09:15 +0200

    At this point I don't even know

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { TaskComponent } from './components/task/task.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
 import { ButtonComponent } from './components/button/button.component';
 import { ImageComponent } from './components/image/image.component';
+import { TaskCardComponent } from './components/task-card/task-card.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    TaskComponent,
+    TasksComponent,
     HeaderComponent,
     FooterComponent,
     AddTaskComponent,
     ButtonComponent,
-    ImageComponent
+    ImageComponent,
+    TaskCardComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
